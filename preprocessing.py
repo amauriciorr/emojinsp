05598a0d86f932dbe28b6bbcf1ea3c6d07033450
Mauricio <robayo.m.antonio@gmail.com>
2021-03-21 15:31:50 -0400

    add function for shuffling sentence pairs, include output dataset

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import re
 import pandas as pd
 import pickle as pkl
+import numpy as np
 from datetime import datetime as dt
 from emoji_util import load_emojis
 
@@ -18,6 +19,8 @@ def get_raw_tweets(file='tweets.txt'):
 def keep_tweet(tweet):
     keep = True
     if URL_REGEX.search(tweet) or HASHTAG_REGEX.search(tweet):
+        keep = False
+    if len(tweet.split(' ')) < 3:
         keep = False
     return keep
 
@@ -46,14 +49,42 @@ def create_df(raw_tweets, emoji_regex):
     print('{} | Cleaning tweets'.format(dt.now()))
     filtered_df['tweets'] = filtered_df.tweets.apply(lambda row: clean_tweet(row, emoji_regex))
     print('{} | Complete.'.format(dt.now()))
+    return filtered_df
     filtered_df.to_csv('processed_tweets.csv', index=False)
+
+def shuffle_pairs(tweet, df, index):
+    correct_pair = set(list(tweet))
+    similar = True
+    while similar:
+        idx = np.random.choice(index, 1).item()
+        incorrect_pair = df.iloc[idx].emoji_sentence
+        incorrect_pair_emojis = set(list(incorrect_pair))
+        if not correct_pair.intersection(incorrect_pair_emojis):
+            similar = False
+    return incorrect_pair
+
+def create_shuffled_df(df):
+    shuffled_df = df.copy()
+    df_index = len(shuffled_df.index)
+    print('{} | Creating incorrect sentence pairs'.format(dt.now()))
+    incorrect_pairs = shuffled_df.emoji_sentence.apply(lambda row: shuffle_pairs(row, shuffled_df, df_index))
+    print('{} | Finished creating incorrect pairs.'.format(dt.now()))
+    shuffled_df['emoji_sentence'] = incorrect_pairs
+    shuffled_df['follows?'] = 0
+    return shuffled_df
 
 if __name__ == '__main__':
     raw_tweets = get_raw_tweets()
     emoji_list, _ = load_emojis()
     emojis =  '|'.join(emoji_list) 
-    # excluding asterisk emoji (*) since it
+    # excluding asterisk emoji *️⃣ since it
     # is interpreted as wildcard and causes more trouble than it's worth
     emojis = emojis[:3569] + emojis[3574:]
     EMOJI_REGEX = re.compile(emojis)
-    create_df(raw_tweets, EMOJI_REGEX)
+    emojinsp_df = create_df(raw_tweets, EMOJI_REGEX)
+    emojinsp_df['follows?'] = 1
+    shuffled_emojinsp_df = create_shuffled_df(emojinsp_df)
+    emoji_frames = [emojinsp_df, shuffled_emojinsp_df]
+    complete_emoji_df = pd.concat(emoji_frames)
+    complete_emoji_df.dropna(inplace=True)
+    complete_emoji_df.to_csv('emoji_nsp_dataset.csv', index = False)
