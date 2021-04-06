3ca06910c16bf4a72f478a13cf64929f94281ba3
Mauricio <robayo.m.antonio@gmail.com>
2021-04-05 21:12:58 -0400

    add device logic, account for embedding resize

--- bertmoji_utils.py
+++ bertmoji_utils.py
@@ -3,13 +3,25 @@ from bertmoji_model import *
 MODEL = "cardiffnlp/twitter-roberta-base"
 
 def load_model(checkpoint_path):
-    checkpoint = torch.load(checkpoint_path)
+    '''
+    helper function for loading model, training + validation performance
+    and tokenizer in one-go
+    '''
+    if torch.cuda.is_available():
+        checkpoint = torch.load(checkpoint_path)
+    else:
+        checkpoint = torch.load(checkpoint_path, map_location='cpu')
     train_performance = checkpoint['train_performance']
     valid_performance = checkpoint['valid_performance']
     model = AutoModel.from_pretrained(MODEL)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL)
+    # necessary for correctly loading checkpoint
+    tokenizer.add_tokens(["[USER]"])
+    model.resize_token_embeddings(len(tokenizer)) 
+
     bertmoji_classifier = bertmoji(model)
     bertmoji_classifier.load_state_dict(checkpoint['model'])
-    return bertmoji_classifier, train_performance, valid_performance
+    return bertmoji_classifier, tokenizer, train_performance, valid_performance
 
 def tokenize_data(data, tokenizer, return_df=False):
     '''
@@ -32,11 +44,14 @@ def tokenize_data(data, tokenizer, return_df=False):
     else:
         return TensorDataset(tokenized_tweets, attn_masks)
 
-def run_batch(model, data, tokenizer):
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+def run_batch(model, data, tokenizer, device):
+    '''
+    wrap tokenize_data in a single test-execution
+    '''
     dataset, df = tokenize_data(data, tokenizer, return_df=True)
     loader = DataLoader(dataset, batch_size=1)
     model.eval()
+    model.to(device)
     predictions = []
     for step, batch in enumerate(loader):
         batch = tuple(t.to(device) for t in batch)
